5ae44d064cfdc292ae8d2ba982e80e0a86c2e899
Aleksandr <tsepuh.mamoru@gmail.com>
2023-06-14 15:46:09 +0200

    final result

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-"# hangman" 
+# Hangman Game
+
+    _________
+   |        |
+   |       \O/
+   |        |
+   |       / \
+  _|_
+
+## Description
+The Hangman Game is a classic word-guessing game implemented in Python. The goal of the game is to guess a hidden word by suggesting letters within a limited number of attempts. This project serves as an opportunity to reinforce programming principles such as loops, conditions, functions, and object-oriented programming (OOP) concepts.
+
+## Installation
+To use the Hangman Game, the following requirements must be met:
+
+- Python 3.x installed on your machine.
+
+1. Clone the repository or download the source code files.
+2. Open a terminal or command prompt and navigate to the project directory.
+3. Run the following command to start the game:
+
+## Usage
+1. Upon running the game, you will be presented with a blank word representing the hidden word to be guessed and the number of remaining attempts.
+2. Guess a letter by entering it in the terminal.
+3. If the letter is present in the word, it will be revealed in the corresponding positions. Otherwise, the number of remaining attempts will decrease.
+4. Continue guessing letters until you guess the entire word correctly or run out of attempts.
+5. The game will end when you either win by guessing the word correctly or lose by running out of attempts.
+
+## Timeline
+The project took approximately 6-7 hours to complete, including the design, implementation, and testing phases. It was developed as part of the AI Bootcamp at BeCode.org, with the aim of strengthening Python syntax and applying programming principles.
+
+## Personal Situation
+This project was undertaken as part of the AI Bootcamp at BeCode.org. The bootcamp provides a comprehensive learning experience in AI concepts and programming. The Hangman Game project was a practical exercise to reinforce programming skills and showcase the application of basic principles and OOP concepts.
+
+
+
+
+
--- utils/game.py
+++ utils/game.py
@@ -1,46 +1,66 @@
+import random
+
 class Hangman:
     def __init__(self):
-        self.possible_words = ['BeCode']
-        self.word_to_find = self.possible.words
-        self.lives = 5
-        self.correctly_guessed_letters = [" _ "] * len(self.word_to_find)
-        self.wrongly_guessed_letters = []
-        self.turn_count = 0
-        self.error_count = 0
- 
-    
-    def play(self):
-        self.user_input = input("Add ONLY letter: ")
-        if self.user_input.isalpha() & len(self.user_input) == 1:
-            for index, letter in enumerate(self.word_to_find):
-                if letter == self.user_input:
-                    self.correctly_guessed_letters[index] = letter
-                else:
-                    self.wrongly_guessed_letters.append(letter)
-                    self.lives -= 1
-                    self.error_count += 1  
+        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
+        self.word_to_find = random.choice(self.possible_words).lower()  # Select a random word from the list
+        self.correctly_guessed_letters = list("_" * len(self.word_to_find))  # Create a list to store correctly guessed letters
+        self.wrongly_guessed_letters = []  # Create a list to store wrongly guessed letters
+        self.lives = 5  # Set the number of lives
+        self.turn_count = 0  # Initialize the turn count
+        self.error_count = 0  # Initialize the error count
 
-                self.turn_count += 1 
+    def play(self):
+        while True:
+            self.user_input = input("Add ONLY letter (type 'quit' to exit): ").lower()  # Get user input
+            if self.user_input == 'quit':  # Check if the user wants to exit
+                self.quit_game()
+                break
+            if len(self.user_input) == 1 and self.user_input.isalpha():  # Check if the user input is a single letter
+                break
+            else:
+                print("Invalid input! Please enter a single letter.")
+
+        if self.user_input in self.word_to_find:  # Check if the user input is in the word
+            for i, char in enumerate(self.word_to_find):  # Iterate over the word
+                if char == self.user_input:  # If the character matches the user input
+                    self.correctly_guessed_letters[i] = self.user_input  # Update the correctly guessed letters list
         else:
-            print("Your input is not a letter")
+            if self.user_input not in self.wrongly_guessed_letters:  # If the user input is not already in the wrongly guessed letters list
+                self.wrongly_guessed_letters.append(self.user_input)  # Add the user input to the wrongly guessed letters list
+                self.lives -= 1  # Decrease the number of lives
+                self.error_count += 1  # Increase the error count
+
+        self.turn_count += 1  # Increase the turn count
 
     def start_game(self):
-        print("Welcome to Hangman!😎")
+        print(f"Welcome to Hangman game☠️! Word to find has {len(self.word_to_find)} letters")
         while True:
-            self.play()
-            if "_" not in self.correctly_guessed_letters:
+            self.play()  # Call the play() method to get user input and update game state
+
+            if self.user_input == 'quit':  # Check if the user wants to quit
+                break
+
+            if "_" not in self.correctly_guessed_letters:  # If there are no more underscores in the correctly guessed letters list
+                self.well_played()  # Call the well_played() method
+                break
+
+            if self.lives == 0:  # If the number of lives reaches 0
+                self.game_over()  # Call the game_over() method
+                break
+
+            # Display game information to the user
+            print(f"Your correctly guessed letters: {' '.join(self.correctly_guessed_letters)}")
+            print(f"Your bad guessed letters: {self.wrongly_guessed_letters}")
+            print(f"Your lives: {self.lives}")
+            print(f"Your error count: {self.error_count}")
+            print(f"Your turn count: {self.turn_count}")
 
     def game_over(self):
-        print("geme over...")
+        print("Game over... The word was:", self.word_to_find)
 
-            
+    def well_played(self):
+        print(f"You found the word: {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
 
-
-            
-
-
-        
-
-
-        
-
+    def quit_game(self): # you didn't mention this method in the game mission but i found it useful😉
+        print("Quitting the game...")
